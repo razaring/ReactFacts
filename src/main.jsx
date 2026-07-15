085b44d05feece59c480b2ac1b04cc44632eb1bf
razaring <razaring@gmail.com>
2026-07-15 08:49:16 -0400

    reactfacts dev work, part 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,6 @@
 import { createRoot } from "react-dom/client";
-import reactLogo from "./assets/react.svg";
+import { App } from "./App";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(
-  <main>
-    <img src={reactLogo} width="40px" alt="React logo" />
-    <h1>Fun facts about React!</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 200K stars on GitHub</li>
-      <li>Is maintained by Meta</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </main>,
-);
+root.render(<App />);
